e7d91174b0e9bce4ef362609539d7d0a37d4737c
Isabella <Bean6754@users.noreply.github.com>
2023-10-03 17:01:10 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,14 +21,47 @@
       (append '((".*\\.php\\'" . html-mode))
               auto-mode-alist))
 
+;; Windmove window controls.
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+
 ;; Theme.
-(setq theme-file "~/.emacs.d/theme.el")
+(setq theme-file "~/.config/emacs/theme.el")
 (load theme-file)
 
 ;; Evil mode.
-;; Enable MELPA for initial evil mode installation ('M-x package-refresh-contents' then 'M-x package-install RET evil').
-;; (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;; (package-initialize)
+;; Enable MELPA to install evil mode and neotree. ('M-x package-refresh-contents' then 'M-x package-install RET <pkg>').
+;; Current pkg list: dired-sidebar, evil and evil-collection.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+;; Evil mode.
+(setq evil-want-keybinding nil)
 (require 'evil)
 (evil-mode 1)
+(require 'evil-collection)
+
+;; Dired-sidebar.
+(add-hook 'emacs-startup-hook #'dired-sidebar-toggle-sidebar)
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'none)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
+  (setq dired-sidebar-width 40)
+  (setq dired-sidebar-display-alist '((side . right)))
+  (setq dired-sidebar-pop-to-sidebar-on-toggle-open nil)
